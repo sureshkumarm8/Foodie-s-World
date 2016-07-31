9415dfa9ff39666eecf28ab71405ad312a985579
Brian Lusina <lusinabrian@gmail.com>
2016-07-31 14:50:40 +0300

    Add Splash screen description

---
+++ app/src/main/java/com/myfood/foodie/SplashScreen.java
@@ -0,0 +1,14 @@
+package com.myfood.foodie;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ Project: Foodie
+ Package: ${PACKAGE_NAME}
+ Created by lusinabrian on 31/07/16 at 14:48
+
+ Description: App life begins here. A simple splash screen. Takes 3 seconds, before moving on to next activity
+ */
+public class SplashScreen extends AppCompatActivity{
+
+}/*CLASS END*/
