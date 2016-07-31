8d12aa8a70cde02e5227d233cd5b4fd3433d1806
Brian Lusina <lusinabrian@gmail.com>
2016-07-31 15:42:27 +0300

    Add Splash Screen
    Set the window to full screen using requestWindow feature set the height and the window and
    initialize the UI controls, that is the app icon, name and tag
    Set the timer on a thread to mock a loading view
    set the splash screen to display for only 3s, before moving on to Main activity

--- app/src/main/java/com/myfood/foodie/SplashScreen.java
+++ app/src/main/java/com/myfood/foodie/SplashScreen.java
@@ -1,8 +1,13 @@
 package com.myfood.foodie;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  Project: Foodie
@@ -13,12 +18,53 @@ import android.support.v7.app.AppCompatActivity;
  Has simple display of app logo, tag and indeterminate progress bar
  */
 public class SplashScreen extends AppCompatActivity{
+    /*FIELDS*/
+    private TextView appName, appTag;
+    private ImageView appIcon;
 
+    /**Set the window to full screen using requestWindow feature
+     * set the height and the window and the view
+     * initialize the UI controls, that is the app icon, name and tag
+     * Set the timer on a thread to mock a loading view
+     * set the splash screen to display for only 3s, before moving on to Main activity*/
     @Override
     public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.splashscreen);
+
+        initializeUICtrls();
+
+        //set the timer
+        Thread timer = new Thread(){
+            @Override
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch(InterruptedException ie){
+                    ie.printStackTrace();
+                }
+                Intent openMain = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(openMain);
+            }
+        };
+        //start timer
+        timer.start();
+    }
+    /**Initializes the User Interface controls
+     * obtains the appName, appTag and appIcon variables and finds their ids in the layout*/
+    private void initializeUICtrls() {
+        appIcon = (ImageView)findViewById(R.id.app_icon_splash);
+        appName = (TextView)findViewById(R.id.app_name_splash);
+        appTag = (TextView)findViewById(R.id.app_tag_splash);
     }
 
-
+    /*kill this splash screen to save memory*/
+    @Override
+    protected void onPause(){
+        super.onPause();
+        finish();
+    }
 }/*CLASS END*/
