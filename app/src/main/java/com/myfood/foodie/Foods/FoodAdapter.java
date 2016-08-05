6da011db2959e2561e4bf3aa569a80fd18c29aaa
Brian Lusina <lusinabrian@gmail.com>
2016-08-05 06:32:38 +0300

    Add Food Adapter
    This adapter will inflate each card item by keeping appropriate information. It will be a subclass of RecyclerView Adapter

---
+++ app/src/main/java/com/myfood/foodie/Foods/FoodAdapter.java
@@ -0,0 +1,13 @@
+package com.myfood.foodie.Foods;
+
+/**
+ * Project: Foodie
+ * Package: com.myfood.foodie.Foods
+ * Created by lusinabrian on 05/08/16 at 06:31
+ * <p/>
+ * Description:Render the RecyclerView, we need an adapter class which inflates the fooditem_layout.xml by keeping appropriate information.
+ */
+public class FoodAdapter {
+
+/*CLASS END, get out!!*/
+}
