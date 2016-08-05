7e9d9470618d49d70f69aadd316db18af7453c4b
Brian Lusina <lusinabrian@gmail.com>
2016-08-05 05:36:55 +0300

    Add Model class and Bumptech dependency
    Model class will help in creating the properties of a single food item that will be instantiated on a card view for each food item. The Bumptech
    dependecy will be used to load the images of each food item on to a single card view.

---
+++ app/src/main/java/com/myfood/foodie/Foods/FoodModel.java
@@ -0,0 +1,17 @@
+package com.myfood.foodie.Foods;
+
+/**
+ * Project: Foodie
+ * Package: com.myfood.foodie.Foods
+ * Created by lusinabrian on 05/08/16 at 05:33
+ * <p/>
+ * Description: Model class that creates an instance of a single food item. This will contain the properties of a single food item, such as name, date posted, user who posted it, recipe, ingredients
+ */
+public class FoodModel {
+    /*constructor*/
+    public FoodModel(){}
+
+
+
+/*CLASS END*/
+}
