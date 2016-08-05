24d7829daf5b323568669fa263303f33305a075d
Brian Lusina <lusinabrian@gmail.com>
2016-08-05 05:44:51 +0300

    Add Constructor and access methods in Food Model
    Constructor and access methods will be used to instantiate each food item in a card view. These properties will also be displayed when the user selects
    a single card(food item)

--- app/src/main/java/com/myfood/foodie/Foods/FoodModel.java
+++ app/src/main/java/com/myfood/foodie/Foods/FoodModel.java
@@ -1,17 +1,61 @@
 package com.myfood.foodie.Foods;
 
+
 /**
  * Project: Foodie
  * Package: com.myfood.foodie.Foods
  * Created by lusinabrian on 05/08/16 at 05:33
  * <p/>
- * Description: Model class that creates an instance of a single food item. This will contain the properties of a single food item, such as name, date posted, user who posted it, recipe, ingredients
+ * Description: Model class that creates an instance of a single food item. This will contain the properties of a single food item, such as name, date posted, user who posted it(chef), recipe, ingredients, brief description
  */
 public class FoodModel {
+    /*fields*/
+    private String foodName, chefName, briefDesc;
+    private int thumbnail;
+
     /*constructor*/
     public FoodModel(){}
 
+    /*constructor*/
+    public FoodModel(String foodName, String chefName, String briefDesc, int thumbnail) {
+        this.foodName = foodName;
+        this.chefName = chefName;
+        this.briefDesc = briefDesc;
+        this.thumbnail = thumbnail;
+    }
 
+/*ACCESS METHODS*/
 
+    public String getFoodName() {
+        return foodName;
+    }
+
+    public void setFoodName(String foodName) {
+        this.foodName = foodName;
+    }
+
+    public String getChefName() {
+        return chefName;
+    }
+
+    public void setChefName(String chefName) {
+        this.chefName = chefName;
+    }
+
+    public String getBriefDesc() {
+        return briefDesc;
+    }
+
+    public void setBriefDesc(String briefDesc) {
+        this.briefDesc = briefDesc;
+    }
+
+    public int getThumbnail() {
+        return thumbnail;
+    }
+
+    public void setThumbnail(int thumbnail) {
+        this.thumbnail = thumbnail;
+    }
 /*CLASS END*/
 }
