51d089ac8fbac6a7f154838d0981df1bd45dda29
Brian Lusina <lusinabrian@gmail.com>
2016-08-07 17:53:11 +0300

    Add FoodViewHolder that extends ViewHolder
    This finds the UI controls by their ids. Essentially holds the UI controls

--- app/src/main/java/com/myfood/foodie/Foods/FoodAdapter.java
+++ app/src/main/java/com/myfood/foodie/Foods/FoodAdapter.java
@@ -1,13 +1,47 @@
 package com.myfood.foodie.Foods;
 
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.test.suitebuilder.TestMethod;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.myfood.foodie.R;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
+
 /**
  * Project: Foodie
  * Package: com.myfood.foodie.Foods
  * Created by lusinabrian on 05/08/16 at 06:31
- * <p/>
  * Description:Render the RecyclerView, we need an adapter class which inflates the fooditem_layout.xml by keeping appropriate information.
+ *
+ * The mContext shall render the views in the appropriate view in which it is called
+ * foodModelList will hold the Food items in a list.
+ *
+ * the FoodViewHolder will find the view items with their ids
  */
-public class FoodAdapter {
+public class FoodAdapter extends RecyclerView.Adapter<FoodAdapter.FoodViewHolder>{
+    private Context mContext;
+    private List<FoodModel> foodModelList;
 
+    public class FoodViewHolder extends RecyclerView.ViewHolder{
+        public ImageView foodImage;
+        public TextView foodTitle, foodDesc, datePosted, chefName;
+
+        public FoodViewHolder(View itemView) {
+            super(itemView);
+            foodImage = (ImageView) itemView.findViewById(R.id.food_image_card);
+            foodTitle = (TextView) itemView.findViewById(R.id.food_title_card);
+            foodDesc = (TextView) itemView.findViewById(R.id.food_desc_card);
+            datePosted = (TextView) itemView.findViewById(R.id.food_date_card);
+            chefName = (TextView) itemView.findViewById(R.id.food_chef_card);
+        }
+    }
+
+    public FoodAdapter
 /*CLASS END, get out!!*/
 }
